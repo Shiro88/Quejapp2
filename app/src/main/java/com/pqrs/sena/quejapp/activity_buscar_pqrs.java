f0ea650574f3db5af96b69d4ef9b68f279b183a0
chaquen <edgar.guzman21@gmail.com>
2016-04-17 14:54:54 -0500

    Se agrega funcionalidad del menu contextual para las actividades buscar y registrar, se crear funciones para enviar mensajes Toast y para lanzar otra actividad

--- app/src/main/java/com/pqrs/sena/quejapp/activity_buscar_pqrs.java
+++ app/src/main/java/com/pqrs/sena/quejapp/activity_buscar_pqrs.java
@@ -1,7 +1,12 @@
 package com.pqrs.sena.quejapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class activity_buscar_pqrs extends AppCompatActivity {
 
@@ -10,4 +15,39 @@ public class activity_buscar_pqrs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_buscar_pqrs);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.mi_menu_buscar, m);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem mItem){
+        switch(mItem.getItemId()){
+            case R.id.mi_item_menu_login:
+                enviarMensajeToast("uno", Toast.LENGTH_SHORT);
+                cambiarActividad(MainActivity.class);
+                break;
+            case R.id.mi_item_menu_registrar:
+                enviarMensajeToast("Ya estas aqui", Toast.LENGTH_SHORT);
+                cambiarActividad(registrar_pqrs.class);
+                break;
+            case R.id.mi_item_menu_buscar:
+                enviarMensajeToast("Ya estas aqui",Toast.LENGTH_SHORT);
+
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+    /*Funcion para enviar mensajes de tipoToast*/
+    public void enviarMensajeToast(String msn,int tiempoDuracion){
+        Toast.makeText(getApplicationContext(),msn,tiempoDuracion).show();
+    }
+    /*Funcion para enviar a otra actividad*/
+    public void cambiarActividad(Class<?> clDestino){
+        startActivity(new Intent(getApplicationContext(), clDestino));
+    }
+
+
 }
