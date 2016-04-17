b2d9dff8b98228ccfd6b9067cbe45154b6888120
Shiro88 <edwarcas@misena.edu.co>
2016-04-17 12:30:14 -0500

    Se agregan Strings

--- app/src/main/java/com/pqrs/sena/quejapp/MainActivity.java
+++ app/src/main/java/com/pqrs/sena/quejapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.pqrs.sena.quejapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //Esta variable es global dentro de esta clase
+Button btnIngresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnIngresar = (Button) findViewById(R.id.btnIngresar);
+        //Aqui agrego el evento onclick
+        if (btnIngresar != null) {
+            btnIngresar.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this,registrar_pqrs.class));
     }
 }
