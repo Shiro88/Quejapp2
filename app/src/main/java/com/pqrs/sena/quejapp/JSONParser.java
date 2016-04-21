7eff6c2fab6c6e38e3fe2bbb8774cef8188abbdb
Shiro88 <edwarcas@misena.edu.co>
2016-04-20 20:09:38 -0500

    Layout registro usuario

---
+++ app/src/main/java/com/pqrs/sena/quejapp/JSONParser.java
@@ -0,0 +1,24 @@
+package com.pqrs.sena.quejapp;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+/**
+ * Created by ColsutecR on 20/04/2016.
+ */
+public class JSONParser extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
+}
