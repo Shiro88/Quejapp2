47e4a1c72cc756640dc7e3f3f503d95d4798a703
Shiro88 <edwarcas@misena.edu.co>
2016-05-08 10:01:38 -0500

    Se crea la clase persona

---
+++ app/src/main/java/com/pqrs/sena/quejapp/Persona.java
@@ -0,0 +1,28 @@
+package com.pqrs.sena.quejapp;
+
+/**
+ * Created by Usaurio on 08/05/2016.
+ */
+public class Persona {
+    public static final String TABLA="Persona";
+    public static final String CLAVE_NOMBRE="Nombres";
+    public static final String CLAVE_APELLIDOS="Apellidos";
+    public static final String CLAVE_IDENTIFICACION="Identificacion";
+    public static final String CLAVE_TIPO_IDENTIFICACION="TipoIdentificacion";
+    public static final String CLAVE_CORREO="Correo";
+    public static final String CLAVE_DIRECCION="Direccion";
+    public static final String CLAVE_GENERO="Genero";
+
+
+    public int Id_Persona;
+    public String strNombres;
+    public String strApellidos;
+    public String strIdentificacion;
+    public String strTipoIdentificacion;
+    public String strCorreo;
+    public String strDireccion;
+    public String strGenero;
+
+
+
+}
