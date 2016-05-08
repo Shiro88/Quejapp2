1b03b9c76440f5bf407f568cf881c15bcc37d07d
Shiro88 <edwarcas@misena.edu.co>
2016-05-08 11:41:50 -0500

    Se crea la clase persona

---
+++ app/src/main/java/com/pqrs/sena/quejapp/MNotificacion.java
@@ -0,0 +1,16 @@
+package com.pqrs.sena.quejapp;
+
+/**
+ * Created by Usaurio on 08/05/2016.
+ * Clase para obtener el medio de notificacion de la respuesta
+ */
+public class MNotificacion {
+    public static final String TABLA="mnotificacion";
+    public static final String CLAVE_ID_MEDIO_NOTIFICACION="IdMNotificacion";
+    public static final String CLAVE_DESCRIPCION="Descripcion";
+
+    public int IdMNOtificacion;
+    public String Descripcion;
+
+
+}
