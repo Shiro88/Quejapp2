07d62aa656a92da23ec2814371071e29345bfa80
Adrian Guzman <edgar.guzman21@gmail.com>
2016-05-28 09:53:58 -0500

    Se agrega funcionaliad de clase web service

--- app/src/main/java/com/pqrs/sena/quejapp/registro_usuario.java
+++ app/src/main/java/com/pqrs/sena/quejapp/registro_usuario.java
@@ -12,6 +12,9 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 import cz.msebera.android.httpclient.Header;
@@ -35,10 +38,49 @@ public class registro_usuario extends AppCompatActivity implements View.OnClickL
         spnTipoDocumento = (Spinner) findViewById(R.id.spntipodocumento);
         edtNombreUsuario =(EditText) findViewById(R.id.edt_nombreusuario);
         edtContraseña =(EditText) findViewById(R.id.edt_contraseña);
+        btnRegistrar.setOnClickListener(this);
 
     }
+
+    public void onClick(View v) {
+        //salvar(v);
+        enviarDatosServidor();
+    }
+
+    private void enviarDatosServidor() {
+        Usuario usuario= new Usuario();
+        usuario.setStrNombres(edtNombreCompleto.getText().toString());
+        usuario.setStrApellidos(edtApellidos.getText().toString());
+        usuario.setStrTipoIdentificacion(spnTipoDocumento.getSelectedItem().toString());
+        usuario.setStrIdentificacion(edtNumeroDocumento.getText().toString());
+        usuario.setStrCorreo(edtNombreUsuario.getText().toString());
+        //usuario.setStrGenero();
+        usuario.setStrContrasenia(edtContraseña.getText().toString());
+        WebService i_u = new WebService();
+        i_u.crear_registro(usuario.getRequestParamsInsertar(), new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                //aqui manejo la respuesta del servidor
+                String str= new String(responseBody);
+                try {
+                    JSONObject jobj=new JSONObject(str);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                    Toast.makeText(getApplicationContext(),str,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),Integer.toString(statusCode).toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
+    }
+
     public void salvar(View view){
-       // enviarDatos(edtNombreCompleto.getText().toString(), edtApellidos.getText().toString(), spnTipoDocumento.getSelectedItem().toString(), edtNumeroDocumento.getText().toString(), edtNombreUsuario.getText().toString(), edtContraseña.getText().toString());
+        // enviarDatos(edtNombreCompleto.getText().toString(), edtApellidos.getText().toString(), spnTipoDocumento.getSelectedItem().toString(), edtNumeroDocumento.getText().toString(), edtNombreUsuario.getText().toString(), edtContraseña.getText().toString());
 
         HashMap<String,String> miHash= new HashMap<>();
         miHash.put("nombre",edtNombreCompleto.getText().toString());
@@ -47,8 +89,8 @@ public class registro_usuario extends AppCompatActivity implements View.OnClickL
         miHash.put("numeroDocumento",edtNumeroDocumento.getText().toString());
         miHash.put("nombreUsuario",edtNombreUsuario.getText().toString());
         miHash.put("clave",edtContraseña.getText().toString());
-        enviarDatos("usuario",miHash);
-        
+        enviarDatos("usuario", miHash);
+
     }
 
     public void enviarDatos(String clave,Object value){
@@ -85,15 +127,14 @@ public class registro_usuario extends AppCompatActivity implements View.OnClickL
                     Toast.makeText(registro_usuario.this, "Comunicacion correcta:" + resultado, Toast.LENGTH_LONG).show();
 
                     //txv_cath_error.setText(resultado);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Status Code" + statusCode,Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Status Code" + statusCode, Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Toast.makeText(registro_usuario.this,statusCode + error.getMessage(),Toast.LENGTH_LONG).show();
+                Toast.makeText(registro_usuario.this, statusCode + error.getMessage(), Toast.LENGTH_LONG).show();
                 // txv_cath_error.setText(error.getMessage().toString());
 
             }
@@ -102,9 +143,6 @@ public class registro_usuario extends AppCompatActivity implements View.OnClickL
     }
 
 
-    public void onClick(View v) {
-        salvar(v);
-    }
 }
 
 
